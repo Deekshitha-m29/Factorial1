456927e8e48d0af5295ae3bb7f5eb6acfd6fcee0
Deekshitha-m29 <deekshitha.m04@gmail.com>
2025-08-13 10:34:17 +0530

    first commit

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+class Test {
+    al(int n)
+    {
+        int res = 1, i;
+        for (i = 2; i <= n; i++)
+            res *= i;
+        return res;
+    }
+
+    // main method
+    public static void main(String[] args)
+    {
+        int num = 5;
+        System.out.println("Factorial of " + num + " is "
+                           + factorial(5));
+    }
+}
